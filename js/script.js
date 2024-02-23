25329d56b78531f11223b17f46ea397fcbe7c5c6
Jonathan Quirino <jonathanquirino30@gmail.com>
2024-02-22 21:16:55 -0300

    Começo do Script
    Inicio do script + correção de alguns erros no HTML.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Seleção de elementos
+const multiplicationForm = document.querySelector("#multiplication-form")
+const numberInput = document.querySelector("#number")
+const multiplicationInput = document.querySelector("#multiplicator")
+//Funções
+
+//Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    console.log(multiplicationNumber + multiplicatorNumber)
+})
